1ceb24b91aea7068e26221782cea2d5165eb212c
FeliksWare <feliksware.dev0@gmail.com>
2026-03-07 17:32:30 +0000

    Day 1 Part 2

--- 01/main.hs
+++ 01/main.hs
@@ -1,7 +1,9 @@
 module Main where
 
+import Data.List
+
 data Direction = NORTH | EAST | SOUTH | WEST
-    deriving Show
+    deriving (Show, Eq)
 
 turn :: Char -> (Direction, Int, Int) -> (Direction, Int, Int)
 turn 'R' (NORTH, x, y) = (EAST, x, y)
@@ -12,6 +14,7 @@ turn 'L' (NORTH, x, y) = (WEST, x, y)
 turn 'L' (EAST, x, y) = (NORTH, x, y)
 turn 'L' (SOUTH, x, y) = (EAST, x, y)
 turn 'L' (WEST, x, y) = (SOUTH, x, y)
+turn _  (d, x, y) = (d, x, y)
 
 move :: Int -> (Direction, Int, Int) -> (Direction, Int, Int)
 move d (NORTH, x, y) = (NORTH, x, y + d)
@@ -21,6 +24,10 @@ move d (WEST, x, y) = (WEST, x - d, y)
 
 followInstruction :: (Direction, Int, Int) -> (Char, Int) -> (Direction, Int, Int)
 followInstruction s (t, d) = (move d) . (turn t) $ s
+
+followInstructionAll :: (Direction, Int, Int) -> (Char, Int) -> [(Direction, Int, Int)]
+followInstructionAll s (t, 0) = []
+followInstructionAll s (t, d) = let ns = (move 1) . (turn t) $ s in followInstructionAll ns ('_', d - 1) ++ [ns]
 
 breakOn :: Eq a => [a] -> [a] -> ([a],[a])
 breakOn d xs@[] = (xs, xs)
@@ -44,7 +51,17 @@ parse = map parseExpr . splitOn ", "
 part1 :: [(Char, Int)] -> Int
 part1 instructions = let (d, x, y) = foldl followInstruction (NORTH, 0, 0) instructions in abs x + abs y
 
+part2 :: [(Char, Int)] -> Int
+part2 instructions = let Just (d, x, y) = firstDuplicate [] $ reverse $ foldl followInstructionAppend [(NORTH, 0, 0)] instructions in abs x + abs y
+    where
+        followInstructionAppend xs@(x:xs') y = (followInstructionAll x y) ++ xs
+        firstDuplicate _ [] = Nothing
+        firstDuplicate seen ((d, x, y):xs) = case find (\ (d', x', y') -> (x == x') && (y == y')) seen of
+            Nothing -> firstDuplicate ((d, x, y):seen) xs
+            otherwise -> Just (d, x, y)
+
 main :: IO ()
 main = do
-    x <- getLine
-    print $ part1 $ parse x
+    source <- getLine
+    putStrLn $ "Part 1: " ++ (show $ part1 $ parse source)
+    putStrLn $ "Part 2: " ++ (show $ part2 $ parse source)
