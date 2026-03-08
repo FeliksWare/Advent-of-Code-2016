355c50bc064c0664b4b10159a82e9072ac5a2523
FeliksWare <feliksware.dev0@gmail.com>
2026-03-08 20:30:24 +0000

    Cleand up the code

--- 04/main.hs
+++ 04/main.hs
@@ -2,8 +2,12 @@ module Main where
 
 import qualified Data.Map as Map
 import qualified Data.List as List
+import qualified Data.Maybe as Maybe
 
 import Data.Char
+
+extractName (name, _) = name
+extractId (_, (id, _)) = id
 
 splitBy :: (a -> Bool) -> [a] -> [[a]]
 splitBy f s =  case dropWhile f s of
@@ -32,20 +36,18 @@ isReal (name, (_, checksum)) = (map extractChar $ take 5 $ List.sortBy compareFr
 
 part1 :: [([String], (Int, String))] -> Int
 part1 = sum . map extractId . filter isReal
-    where
-        extractId (_, (id, _)) = id
 
-decrypt :: ([String], (Int, String)) -> (String, (Int, String))
-decrypt (name, (id, checksum)) = (map (shiftN id) $ unwords name, (id, checksum))
+decrypt :: Int -> String -> String
+decrypt key = map (shiftN key)
     where
         shiftN n c | isAlpha c && isLower c = chr $ mod (ord c - ord 'a' + n) 26 + ord 'a' 
         shiftN _ c = c
 
 part2 :: [([String], (Int, String))] -> Int
-part2 = extractId . head . filter ((=="northpole object storage") . extractName) . map decrypt . filter isReal
+part2 = extractId . findRoom . map decryptName . filter isReal
     where
-        extractName (name, _) = name
-        extractId (_, (id, _)) = id
+        decryptName (name, (id, checksum)) = (decrypt id $ unwords name, (id, checksum))
+        findRoom = Maybe.fromJust . List.find ((=="northpole object storage") . extractName)
 
 main :: IO ()
 main = do
