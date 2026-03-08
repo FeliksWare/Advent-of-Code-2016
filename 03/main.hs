e96cdbbcf874dcf535a6028946731176cb73b43b
FeliksWare <feliksware.dev0@gmail.com>
2026-03-08 00:16:44 +0000

    Day 3 Part 2

--- 03/main.hs
+++ 03/main.hs
@@ -3,16 +3,24 @@ module Main where
 readInt :: String -> Int
 readInt = read
 
-parse :: String -> [(Int, Int, Int)]
-parse = map ((\(a:b:c:_) -> (a, b, c)) . map readInt . words) . lines
+parse1 :: String -> [(Int, Int, Int)]
+parse1 = map (parseExpr . map readInt . words) . filter (/="") . lines
+    where parseExpr (a:b:c:_) = (a, b, c)
+
+parse2 :: String -> [(Int, Int, Int)]
+parse2 = parseExpr . map (map readInt . words) . filter (/="") . lines
+    where
+        parseExpr [] = []
+        parseExpr ((a:b:c:_):(d:e:f:_):(g:h:i:_):r) = [(a, d, g), (b, e, h), (c, f, i)] ++ parseExpr r
 
 validTriangle :: (Int, Int, Int) -> Bool
 validTriangle (a, b, c) = a + b > c && b + c > a && a + c > b
 
-part1 :: [(Int, Int, Int)] -> Int
-part1 = length . filter validTriangle
+solve :: [(Int, Int, Int)] -> Int
+solve = length . filter validTriangle
 
 main :: IO ()
 main = do
-    source <- parse <$> getContents
-    putStrLn $ "Part 1: " ++ (show $ part1 source)
+    source <- getContents
+    putStrLn $ "Part 1: " ++ (show $ solve $ parse1 source)
+    putStrLn $ "Part 2: " ++ (show $ solve $ parse2 source)
