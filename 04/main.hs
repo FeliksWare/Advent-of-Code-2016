7b5f1894fdce74c293e850999b6e885ebcbd2e89
FeliksWare <feliksware.dev0@gmail.com>
2026-03-08 19:50:33 +0000

    Day 4 Part 1

---
+++ 04/main.hs
@@ -0,0 +1,40 @@
+module Main where
+
+import qualified Data.Map as Map
+import qualified Data.List as List
+
+splitBy :: (a -> Bool) -> [a] -> [[a]]
+splitBy f s =  case dropWhile f s of
+    [] -> []
+    s' -> w : splitBy f s''
+        where (w, s'') = break f s'
+
+wordsBy :: Char -> String -> [String]
+wordsBy c = splitBy (==c)
+
+parse :: String -> [([String], (Int, String))]
+parse = map (parseExpr . wordsBy '-') . filter (/="") . lines
+    where
+        parseExpr s = (init s, parseIdAndChecksum $ splitIdAndChecksum $ last s)
+        splitIdAndChecksum = splitBy (\x -> x == '[' || x == ']')
+        parseIdAndChecksum (a:b:_) = (read a, b)
+
+isReal :: ([String], (Int, String)) -> Bool
+isReal (name, (_,checksum)) = (map extractChar $ take 5 $ List.sortBy compareFrequences $ charFrequence $ concat name) == checksum
+    where
+        charFrequence s = Map.toList $ Map.fromListWith (+) [(c, 1) | c <- s]
+        compareFrequences (c1, f1) (c2, f2) = case compare f2 f1 of
+            EQ -> compare c1 c2
+            a -> a
+        extractChar (c, _) = c
+
+
+part1 :: [([String], (Int, String))] -> Int
+part1 = sum . map extractId . filter isReal
+    where
+        extractId (_, (id,_)) = id
+
+main :: IO ()
+main = do
+    source <- getContents
+    putStrLn $ "Part 1: " ++ (show $ part1 $ parse source)
