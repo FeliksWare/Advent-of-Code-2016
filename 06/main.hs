4c52cbd5ea724459b4c099c9a405af65aa8e89fc
FeliksWare <feliksware.dev0@gmail.com>
2026-03-14 02:22:03 +0000

    Day 6 Part 2

--- 06/main.hs
+++ 06/main.hs
@@ -4,20 +4,29 @@ import qualified Data.List as List
 import qualified Data.Map as Map
 
 parse :: String -> [String]
-parse = filter (/="") . lines
+parse = List.transpose . filter (/="") . lines
+
+frequences :: Ord a => [a] -> Map.Map a Int
+frequences xs = Map.fromListWith (+) [(x, 1) | x <- xs]
 
 mostCommon :: Ord a => [a] -> a
-mostCommon xs = extractChar $ List.minimumBy compareFrequences (Map.toList $ Map.fromListWith (+) [(x, 1) | x <- xs])
+mostCommon = extractValue . List.minimumBy compareFrequences . Map.toList . frequences
     where
-        extractChar (c, f) = c
-        compareFrequences (c1, f1) (c2, f2) = case compare f2 f1 of
-            EQ -> compare c1 c2
+        extractValue (v, f) = v
+        compareFrequences (v1, f1) (v2, f2) = case compare f2 f1 of
+            EQ -> compare v1 v2
             t -> t
 
-part1 :: [String] -> String
-part1 = map mostCommon . List.transpose
+leastCommon :: Ord a => [a] -> a
+leastCommon = extractValue . List.minimumBy compareFrequences . Map.toList . frequences
+    where
+        extractValue (v, f) = v
+        compareFrequences (v1, f1) (v2, f2) = case compare f1 f2 of
+            EQ -> compare v1 v2
+            t -> t
 
 main :: IO ()
 main = do
     source <- parse <$> getContents
-    putStrLn $ "Part 1: " ++ show (part1 source)
+    putStrLn $ "Part 1: " ++ show (map mostCommon source)
+    putStrLn $ "Part 2: " ++ show (map leastCommon source)
