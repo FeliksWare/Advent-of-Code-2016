a011da78b9cb7b5f5a614d89dc3de7ebf6935ce6
FeliksWare <feliksware.dev0@gmail.com>
2026-03-21 01:54:50 +0000

    Day 11 Part 2

--- 11/main.hs
+++ 11/main.hs
@@ -6,12 +6,10 @@ import qualified Data.Maybe as Maybe
 import qualified Data.List as List
 import Data.Char
 
-import Debug.Trace
-
 data Item = Generator String | Microchip String
     deriving (Show, Eq, Ord)
 
-type Building = Map.Map Int (Set.Set Item)
+type Building = [Set.Set Item]
 
 splitBy :: (a -> Bool) -> [a] -> [[a]]
 splitBy f s =  case dropWhile f s of
@@ -33,14 +31,14 @@ parseFloorName "second" = 2
 parseFloorName "third" = 3
 parseFloorName "fourth" = 4
 
-parseFloor :: [String] -> (Int, Set.Set Item)
-parseFloor ("The":floor:"floor":"contains":list) = (parseFloorName floor, parseList list)
+parseFloor :: [String] -> Set.Set Item
+parseFloor ("The":floor:"floor":"contains":list) = parseList list
 
-parseLine :: String -> (Int, Set.Set Item)
+parseLine :: String -> Set.Set Item
 parseLine = parseFloor . splitBy (not . isAlpha)
 
 parse :: String -> Building
-parse = Map.fromList . map parseLine . filter (/="") . lines
+parse = map parseLine . filter (/="") . lines
 
 isGenerator :: Item -> Bool
 isGenerator (Generator _) = True
@@ -54,32 +52,40 @@ isSafe s _ = True
 isValid :: Set.Set Item -> Bool
 isValid s = all (isSafe s) s
 
+isValidState :: (Int, Building) -> Bool
+isValidState (_, building) = all isValid building
+
 takeItems :: [Item] -> [Set.Set Item]
 takeItems [] = []
 takeItems (x:xs) = filter isValid (map (Set.insert x . Set.singleton) xs) ++ takeItems xs ++ [Set.singleton x]
 
-elevator :: (Int, Building) -> Int -> Set.Set Item -> Maybe (Int, Building)
-elevator (floor, building) floor' items = case (Map.lookup floor building, Map.lookup floor' building) of
-    (Just from, Just to) -> let (from', to') = (Set.difference from items, Set.union to items) in
-        if isValid from' && isValid to' then Just (floor', Map.insert floor from' $ Map.insert floor' to' building) else Nothing
-    _ -> Nothing
-
+-- horrendous
 nextStates :: (Int, Building) -> Set.Set (Int, Building)
-nextStates state@(floor, building) = let items = maybe [] (takeItems . Set.toList) (Map.lookup floor building) in
-    Set.fromList $ Maybe.mapMaybe (elevator state $ floor + 1) items ++ Maybe.mapMaybe (elevator state $ floor - 1) items
+nextStates state@(1, [a, b, c, d]) = let items = takeItems $ Set.toList a in
+    Set.fromList $ filter isValidState $ map (\e -> (2, [Set.difference a e, Set.union b e, c, d])) items
+nextStates state@(2, [a, b, c, d]) = let items = takeItems $ Set.toList b in
+    Set.fromList $ filter isValidState $ map (\e -> (3, [a, Set.difference b e, Set.union c e, d])) items ++ map (\e -> (1, [Set.union a e, Set.difference b e, c, d])) items
+nextStates state@(3, [a, b, c, d]) = let items = takeItems $ Set.toList c in
+    Set.fromList $ filter isValidState $ map (\e -> (4, [a, b, Set.difference c e, Set.union d e])) items ++ map (\e -> (2, [a, Set.union b e, Set.difference c e, d])) items
+nextStates state@(4, [a, b, c, d]) = let items = takeItems $ Set.toList d in
+    Set.fromList $ filter isValidState $ map (\e -> (3, [a, b, Set.union c e, Set.difference d e])) items
 
 isSolved :: (Int, Building) -> Bool
-isSolved (floor, building) = all (\(k, a) -> if k == 4 then not $ Set.null a else Set.null a) $ Map.assocs building
+isSolved (floor, [a, b, c, d]) = null a && null b && null c && not (null d)
 
-solve :: Int -> Set.Set (Int, Building) -> Set.Set (Int, Building) -> Maybe Int
-solve steps visited states | Set.null states = Nothing
-solve steps visited states | any isSolved states = Just steps
-solve steps visited states = trace (show $ length visited) $ solve (steps + 1) (Set.union visited states) $ foldr (Set.union . flip Set.difference visited . nextStates) Set.empty states
+solve :: Int -> Set.Set (Int, Building) -> ((Int, Building) -> Bool) -> Set.Set (Int, Building) -> Maybe Int
+solve steps visited isSolved states | Set.null states = Nothing
+solve steps visited isSolved states | any isSolved states = Just steps
+solve steps visited isSolved states = solve (steps + 1) (Set.union visited states) isSolved $ foldr (Set.union . flip Set.difference visited . nextStates) Set.empty states
 
 part1 :: Building -> Maybe Int
-part1 building = solve 0 Set.empty $ Set.singleton (1, building)
+part1 building = solve 0 Set.empty (==(1, building)) $ Set.singleton (4, [Set.empty, Set.empty, Set.empty, foldr Set.union Set.empty building])
+
+part2 :: Building -> Maybe Int
+part2 (x:xs) = part1 $ Set.union x (Set.fromList [Generator "elerium", Microchip "elerium", Generator "dilithium", Microchip "dilithium"]) : xs
 
 main :: IO ()
 main = do
     source <- parse <$> getContents
     putStrLn $ "Part 1: " ++ show (part1 source)
+    putStrLn $ "Part 2: " ++ show (part2 source)
