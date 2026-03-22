e176a6d9d5b7cae961ad4067c5b9aa9d2d766495
FeliksWare <feliksware.dev0@gmail.com>
2026-03-22 14:15:06 +0000

    Day 13 Part 2

--- 13/main.hs
+++ 13/main.hs
@@ -21,10 +21,18 @@ solve :: Int -> ((Int, Int) -> Bool) -> (Int, Int) -> Set.Set (Int, Int) -> Int
 solve steps isOpen destination current | Set.member destination current = steps
 solve steps isOpen destination current = solve (steps + 1) isOpen destination $ Set.foldr (Set.union . Set.fromList . filter isOpen . adjacent) current current
 
+locations :: Int -> ((Int, Int) -> Bool) -> (Int, Int) -> Set.Set (Int, Int) -> Set.Set (Int, Int)
+locations steps isOpen destination current | steps <= 0 = current
+locations steps isOpen destination current = locations (steps - 1) isOpen destination $ Set.foldr (Set.union . Set.fromList . filter isOpen . adjacent) current current
+
 part1 :: Int -> Int
 part1 n = solve 0 (isOpen n) (31, 39) $ Set.singleton (1, 1)
+
+part2 :: Int -> Int
+part2 n = length $ locations 50 (isOpen n) (31, 39) $ Set.singleton (1, 1)
 
 main :: IO ()
 main = do
     source <- readInt <$> getContents
     putStrLn $ "Part 1: " ++ show (part1 source)
+    putStrLn $ "Part 2: " ++ show (part2 source)
