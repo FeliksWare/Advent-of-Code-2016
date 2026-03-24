476b74a0ba217d8b1f73cbb165b665ad19702251
FeliksWare <feliksware.dev0@gmail.com>
2026-03-24 09:47:20 +0000

    Improved solution with lcm

--- 15/main.hs
+++ 15/main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.List
 import Data.Char
 
 data Disc = Disc {
@@ -27,14 +28,19 @@ parse = map parseLines . filter (/= "") . lines
 moveDisc :: Int -> Disc -> Disc
 moveDisc n (Disc number positions time position) = Disc number positions (time + n) ((position + n) `mod` positions)
 
-solve :: Int -> [Disc] -> Int
-solve steps discs | all ((==0) . position) discs = steps
-solve steps discs = solve (steps + 1) $ map (moveDisc 1) discs
+solve :: Int -> Int -> [Disc] -> Int
+solve steps n [] = steps
+solve steps n discs | any ((==0) . position) discs = let (solved, unsolved) = partition ((==0) . position) discs in solve steps (foldr (lcm . positions) n solved) unsolved
+solve steps n discs = solve (steps + n) n $ map (moveDisc n) discs
 
 part1 :: [Disc] -> Int
-part1 = solve 0 . map (\d -> moveDisc (number d) d)
+part1 = solve 0 1 . map (\d -> moveDisc (number d) d)
+
+part2 :: [Disc] -> Int
+part2 discs = solve 0 1 $ map (\d -> moveDisc (number d) d) $ discs ++ [Disc (maximum (map number discs) + 1) 11 0 0]
 
 main :: IO ()
 main = do
     source <- parse <$> getContents
     putStrLn $ "Part 1: " ++ show (part1 source)
+    putStrLn $ "Part 2: " ++ show (part2 source)
