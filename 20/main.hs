ba15f47019763e0a4ad5dfb376536c527e1b25d4
FeliksWare <feliksware.dev0@gmail.com>
2026-03-28 13:32:00 +0000

    Day 20 Part 2

--- 20/main.hs
+++ 20/main.hs
@@ -16,7 +16,17 @@ findSmallest bounds value = case find (\(min, max) -> min <= value && value <= m
 part1 :: [(Int, Int)] -> Int
 part1 bounds = findSmallest bounds 0
 
+findCount :: [(Int, Int)] -> Int -> Int -> Int
+findCount bounds value count | value > 4294967295 = count
+findCount bounds value count = case find (\(min, max) -> min <= value && value <= max) bounds of
+    Just (min, max) -> findCount bounds (max + 1) count
+    Nothing -> findCount bounds (value + 1) (count + 1)
+
+part2 :: [(Int, Int)] -> Int
+part2 bounds = findCount bounds 0 0
+
 main :: IO ()
 main = do
     source <- parse <$> getContents
     putStrLn $ "Part 1: " ++ show (part1 source)
+    putStrLn $ "Part 2: " ++ show (part2 source)
