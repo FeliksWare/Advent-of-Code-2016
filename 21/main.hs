8dac4f966cfc21a2444b4cef27c55b13085f3716
FeliksWare <feliksware.dev0@gmail.com>
2026-03-30 02:27:09 +0100

    Day 21 Part 2

--- 21/main.hs
+++ 21/main.hs
@@ -3,6 +3,10 @@ module Main where
 import qualified Data.Sequence as Seq
 import Data.Char
 
+import Debug.Trace
+
+data Ins = SwapPosition Int Int | SwapLetter Char Char | RotateLeft Int | RotateRight Int | RotateBase Char | Reverse Int Int | MovePosition Int Int
+
 splitBy :: (a -> Bool) -> [a] -> [[a]]
 splitBy f s =  case dropWhile f s of
     [] -> []
@@ -29,31 +33,52 @@ reverseSubseq :: Int -> Int -> Seq.Seq Char -> Seq.Seq Char
 reverseSubseq l r s | r < l = s
 reverseSubseq l r s = reverseSubseq (l + 1) (r - 1) $ swapPosition l r s
 
-move :: Int -> Int -> Seq.Seq Char -> Seq.Seq Char
-move from to s = Seq.insertAt to (Seq.index s from) $ Seq.deleteAt from s 
+movePosition :: Int -> Int -> Seq.Seq Char -> Seq.Seq Char
+movePosition from to s = Seq.insertAt to (Seq.index s from) $ Seq.deleteAt from s 
 
-parseIns :: [String] -> (Seq.Seq Char -> Seq.Seq Char)
-parseIns ["swap", "position", x, "with", "position", y] = swapPosition (read x) (read y)
-parseIns ["swap", "letter", x, "with", "letter", y] = swapLetter (head x) (head y)
-parseIns ["rotate", "left", x, "steps"] = flip (!!) (read x) . iterate rotateLeft
-parseIns ["rotate", "right", x, "steps"] = flip (!!) (read x) . iterate rotateRight
-parseIns ["rotate", "left", "1", "step"] = rotateLeft
-parseIns ["rotate", "right", "1", "step"] = rotateRight
-parseIns ["rotate", "based", "on", "position", "of", "letter", x] =
-    \s -> flip (!!) (let Just i = Seq.elemIndexL (head x) s in if i >= 4 then i + 2 else i + 1) $ iterate rotateRight s
-parseIns ["reverse", "positions", x, "through", y] = reverseSubseq (read x) (read y)
-parseIns ["move", "position", x, "to", "position", y] = move (read x) (read y)
+parseIns :: [String] -> Ins
+parseIns ["swap", "position", x, "with", "position", y] = SwapPosition (read x) (read y)
+parseIns ["swap", "letter", x, "with", "letter", y] = SwapLetter (head x) (head y)
+parseIns ["rotate", "left", x, "steps"] = RotateLeft $ read x
+parseIns ["rotate", "right", x, "steps"] = RotateRight $ read x
+parseIns ["rotate", "left", "1", "step"] = RotateLeft 1
+parseIns ["rotate", "right", "1", "step"] = RotateRight 1
+parseIns ["rotate", "based", "on", "position", "of", "letter", x] = RotateBase $ head x
+parseIns ["reverse", "positions", x, "through", y] = Reverse (read x) (read y)
+parseIns ["move", "position", x, "to", "position", y] = MovePosition (read x) (read y)
 
-parseLine :: String -> (Seq.Seq Char -> Seq.Seq Char)
+parseLine :: String -> Ins
 parseLine = parseIns . splitBy isSpace
 
-parse :: String -> [Seq.Seq Char -> Seq.Seq Char]
+parse :: String -> [Ins]
 parse = map parseLine . filter (/="") . lines
 
-part1 :: [Seq.Seq Char -> Seq.Seq Char] -> Seq.Seq Char
-part1 = foldl (\a b -> b a) (Seq.fromList "abcdefgh")
+execIns :: Seq.Seq Char -> Ins -> Seq.Seq Char
+execIns s (SwapPosition x y) = swapPosition x y s
+execIns s (SwapLetter x y) = swapLetter x y s
+execIns s (RotateLeft x) = flip (!!) x $ iterate rotateLeft s
+execIns s (RotateRight x) = flip (!!) x $ iterate rotateRight s
+execIns s (RotateBase x) = flip (!!) (let Just i = Seq.elemIndexL x s in if i >= 4 then i + 2 else i + 1) $ iterate rotateRight s
+execIns s (Reverse x y) = reverseSubseq x y s
+execIns s (MovePosition x y) = movePosition x y s
+
+undoIns :: Ins -> Seq.Seq Char -> Seq.Seq Char
+undoIns (SwapPosition x y) s = swapPosition x y s
+undoIns (SwapLetter x y) s = swapLetter x y s
+undoIns (RotateLeft x) s = flip (!!) x $ iterate rotateRight s
+undoIns (RotateRight x) s = flip (!!) x $ iterate rotateLeft s
+undoIns (RotateBase x) s = head $ dropWhile ((/=s) . flip execIns (RotateBase x)) $ iterate rotateLeft s
+undoIns (Reverse x y) s = reverseSubseq x y s
+undoIns (MovePosition x y) s = movePosition y x s
+
+part1 :: [Ins] -> Seq.Seq Char
+part1 = foldl execIns (Seq.fromList "abcdefgh")
+
+part2 :: [Ins] -> Seq.Seq Char
+part2 = foldr undoIns (Seq.fromList "fbgdceah")
 
 main :: IO ()
 main = do
     source <- parse <$> getContents
     putStrLn $ "Part 1: " ++ show (part1 source)
+    putStrLn $ "Part 2: " ++ show (part2 source)
