6a10743ce355da12be068df16cf917be3cace82b
FeliksWare <feliksware.dev0@gmail.com>
2026-04-07 14:26:02 +0100

    Day 24 Part 2

--- 24/main.hs
+++ 24/main.hs
@@ -26,16 +26,26 @@ toStart = head . Map.keys . Map.filter (=='0')
 adjacent :: (Int, Int) -> [(Int, Int)]
 adjacent (x, y) = [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
 
-solve :: Tiles -> Int -> Set.Set ((Int, Int), Tiles) -> Set.Set ((Int, Int), Tiles) -> Int
-solve grid steps visited current | any (Set.null . snd) current = steps
-solve grid steps visited current = solve grid (steps + 1) (Set.union visited current) $ Set.difference nextStates visited
+solve2 :: (Int, Int) -> Tiles -> Int -> Set.Set ((Int, Int), Tiles) -> Set.Set ((Int, Int), Tiles) -> Int
+solve2 start grid steps visited current | any (\(pos, tiles) -> pos == start && Set.null tiles ) current = steps
+solve2 start grid steps visited current = solve2 start grid (steps + 1) (Set.union visited current) $ Set.difference nextStates visited
+    where
+        nextStates = Set.foldr (Set.union . Set.fromList . \(pos, goals) -> map (\pos' -> (pos', Set.delete pos' goals)) $ filter (`Set.member` grid) $ adjacent pos) Set.empty current
+
+solve1 :: Tiles -> Int -> Set.Set ((Int, Int), Tiles) -> Set.Set ((Int, Int), Tiles) -> Int
+solve1 grid steps visited current | any (Set.null . snd) current = steps
+solve1 grid steps visited current = solve1 grid (steps + 1) (Set.union visited current) $ Set.difference nextStates visited
     where
         nextStates = Set.foldr (Set.union . Set.fromList . \(pos, goals) -> map (\pos' -> (pos', Set.delete pos' goals)) $ filter (`Set.member` grid) $ adjacent pos) Set.empty current
 
 part1 :: Map.Map (Int, Int) Char -> Int
-part1 map = solve (toGrid map) 0 Set.empty (Set.singleton (toStart map, toGoals map))
+part1 map = solve1 (toGrid map) 0 Set.empty (Set.singleton (toStart map, toGoals map))
+
+part2 :: Map.Map (Int, Int) Char -> Int
+part2 map = solve2 (toStart map) (toGrid map) 0 Set.empty (Set.singleton (toStart map, toGoals map))
 
 main :: IO ()
 main = do
     source <- parse <$> getContents
     putStrLn $ "Part 1: " ++ show (part1 source)
+    putStrLn $ "Part 2: " ++ show (part2 source)
